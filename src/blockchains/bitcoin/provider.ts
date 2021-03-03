dfc675090ac331e9ba3bca0ed9a53576dc5080fe
Kingsley Victor <javaprodigy56@gmail.com>
2021-03-03 20:12:04 +0100

    new commit

--- src/blockchains/bitcoin/provider.ts
+++ src/blockchains/bitcoin/provider.ts
@@ -1,16 +1,16 @@
 import { RpcServer } from "../common";
 
 export class BitcoinProvider {
- private _rpc: RpcServer;
+  private _rpc: RpcServer;
 
- constructor(rpcUrl: string) {
-  this._rpc = new RpcServer(rpcUrl);
- }
+  constructor(rpcUrl: string) {
+    this._rpc = new RpcServer(rpcUrl);
+  }
 
- // getBalance(address: string): Promise<any> {
- //  return this._rpc.call({
- //   method: "getbalance",
- //   id: "curltest"
- //  })
- // }
+  // getBalance(address: string): Promise<any> {
+  //  return this._rpc.call({
+  //   method: "getbalance",
+  //   id: "curltest"
+  //  })
+  // }
 }
