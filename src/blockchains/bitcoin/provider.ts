fbc3df283a02f80569858ef1d7f80644e46a7644
Kingsley Victor <javaprodigy56@gmail.com>
2021-03-31 12:59:29 +0100

    prepare bitcoin sendrawtransaction

--- src/blockchains/bitcoin/provider.ts
+++ src/blockchains/bitcoin/provider.ts
@@ -49,8 +49,62 @@ export class BitcoinProvider {
       .then(this.handleResponse);
   }
 
-  // sendRawTransaction(tx: Tx): Promise<any> {
-  // }
+  async sendRawTransaction(
+    tx: Tx,
+    privateKey: string,
+    isDev: boolean = false
+  ): Promise<any> {
+    const res = await fetch(
+      `https://sochain.com/api/v2/get_tx_unspent/${isDev ? "BTCTEST" : "BTC"}/${
+        tx.from
+      }`
+    );
+
+    if (res.status >= 400)
+      throw new Error("Could not get unspent outputs for " + tx.from);
+
+    const unspentTxResponseAsJson = await res.json();
+    const network = isDev ? bitcoin.networks.testnet : bitcoin.networks.bitcoin;
+    const keypair = bitcoin.ECPair.fromWIF(privateKey, network);
+    const payments = bitcoin.payments.p2wpkh({
+      pubkey: keypair.publicKey,
+      network
+    });
+    const txn = new bitcoin.Psbt({ network });
+    let inputs: Array<{
+      hash: string;
+      index: number;
+      value: number;
+      [key: string]: any;
+    }> = [];
+
+    for (const transaction of unspentTxResponseAsJson.data.txs)
+      inputs = inputs.concat({
+        hash: transaction.txid,
+        index: transaction.output_no,
+        script: transaction.script_hex,
+        value: parseFloat(transaction.value)
+      });
+
+    txn
+      .addInputs(
+        inputs.map(i => ({
+          hash: i.hash,
+          index: i.index,
+          nonWitnessUtxo: Buffer.from(i.script, "hex")
+        }))
+      )
+      .addOutputs([
+        {
+          address: tx.to,
+          value: tx.value * Math.pow(10, 8)
+        },
+        {
+          address: payments.address,
+          value: inputs.map(i => i.value).reduce((a, b) => a + b)
+        }
+      ]);
+  }
 
   // getBalance(address: string): Promise<any> {
   //  return this._rpc.call({
